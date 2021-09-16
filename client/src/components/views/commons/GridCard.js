dbb1c4a7d0d8c601a2356cc84b6e2e7fe1cb7249
Gomsonixx <gomsonixx@gmail.com>
2021-09-16 18:54:22 +0900

    Add Movie Details Page

--- client/src/components/views/commons/GridCard.js
+++ client/src/components/views/commons/GridCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Col } from 'antd'
 
 function GridCard(props) {
-    return (
+    if (props.landingPage) {return (
         <Col lg={6} md={8} xs={24}>
             <div style={{ position: 'relative' }}>
                 <a href = {`/movie/${props.movieId}`}>
@@ -10,7 +10,20 @@ function GridCard(props) {
                 </a>
             </div>
         </Col>
-    )
+    )} else {
+        return (
+          <Col lg={6} md={8} xs={24}>
+            <div style={{ position: "relative" }}>
+                <img
+                  style={{ width: "100%", height: "320px" }}
+                  src={props.image}
+                  alt={props.characterName}
+                />
+            </div>
+          </Col>
+        );
+    }
+    
 }
 
 export default GridCard
